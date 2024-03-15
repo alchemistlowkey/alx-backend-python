e114a60eef1d217512e277e90c4f770b8b6e54e2
alchemistlowkey <alchemistlowkey@gmail.com>
2024-03-15 04:07:36 +0000

    list, union and tuple annotation

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,7 +2,17 @@
 """
 A type-annotated function sum_list which takes a list input_list of floats
 """
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
+    """
+    Calculate the sum of a list of floats.
 
+    Parameters:
+        input_list (List[float]): A list of floats.
+
+    Returns:
+        float: The sum of the floats in the input list.
+    """
+    return sum(input_list)
---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+A type-annotated function to_kv that takes a string k and
+an int OR float v as arguments and returns a tuple.
+The first element of the tuple is the string k.
+The second element is the square of the int/float v and
+should be annotated as a float.
+"""
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Create a tuple with the string k and the square of the int/float v.
+
+    Parameters:
+        k (str): The string key.
+        v (Union[int, float]): The integer or float value.
+
+    Returns:
+        Tuple[str, float]: A tuple containing the string k and the square of v as a float.
+    """
+    return k, float(v ** 2)
