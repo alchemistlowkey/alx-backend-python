ca977d6ff349489dd937aa751268f262549aabcd
alchemistlowkey <alchemistlowkey@gmail.com>
2024-03-20 00:30:55 +0000

    modified to use comprehension

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -25,13 +25,7 @@ async def measure_runtime() -> float:
     """
     start_time = time.time()
 
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
-
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
     end_time = time.time()
     total_runtime = end_time - start_time
     return total_runtime
