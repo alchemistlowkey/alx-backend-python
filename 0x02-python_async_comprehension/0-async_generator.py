08276099edb1bec5f2db5e1052cf794da6f0e72e
alchemistlowkey <alchemistlowkey@gmail.com>
2024-03-20 00:23:54 +0000

    modified for return values

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,9 +7,10 @@ then yield a random number between 0 and 10. Use the random module.
 """
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """
     Coroutine that generates random numbers asynchronously.
 
--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -14,7 +14,7 @@ import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime():
+async def measure_runtime() -> float:
     """
     Coroutine that executes async_comprehension four times
     in parallel using asyncio.gather and measures the total runtime.
